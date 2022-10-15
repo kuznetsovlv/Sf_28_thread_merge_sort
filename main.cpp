034aa52aac08a0d2dce4b8d6aa3adf0a599dba09
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2022-10-15 13:03:22 +0300

    Improved testing

--- main.cpp
+++ main.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 #include <ctime>
 #include <random>
+#include <string>
 #include "merge_sort.h"
 
-const size_t SIZE = 10;
+const size_t SIZE = 100000;
+const int LIMIT = 10000;
 
 void print(const int*, const size_t);
 
-int main()
+int main(int argc, char **argvs)
 {
 	srand(time(nullptr));
 
-	int *arr = new int[SIZE];
+	const size_t size = argc > 1 ? std::stoull(*(argvs + 1)) : SIZE;
 
-	for(size_t i = 0; i < SIZE; ++i)
+	int *arr = new int[size];
+
+	for(size_t i = 0; i < size; ++i)
 	{
-		*(arr + i) = rand() % 100;
+		*(arr + i) = rand() % LIMIT;
 	}
 
 	std::cout << "Before sort:" << std::endl;
-	print(arr, SIZE);
+	print(arr, size);
 
-	mergeSort(arr, SIZE);
+	mergeSort(arr, size);
 
 	std::cout << "After sort:" << std::endl;
-	print(arr, SIZE);
+	print(arr, size);
 
 	delete [] arr;
 
